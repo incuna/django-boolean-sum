ca43fdc6ffbbd6d05d3e068a1a923673b7326907
Nicolas Pieuchot <n.pieuchot@zero-gachis.com>
2015-08-18 11:08:46 +0200

    Fixing PEP8

--- django_boolean_sum.py
+++ django_boolean_sum.py
@@ -6,13 +6,16 @@ from django.db.models.sql.aggregates import Sum as BaseSQLSum
 class SQLSum(BaseSQLSum):
     @property
     def sql_template(self):
-        if settings.DATABASES['default']['ENGINE'] == 'django.db.backends.postgresql_psycopg2':
+        if settings.DATABASES['default']['ENGINE'] == \
+                'django.db.backends.postgresql_psycopg2':
             return '%(function)s(%(field)s::int)'
         return '%(function)s(%(field)s)'
 
 
 class BooleanSum(Sum):
     function = None
+
     def add_to_query(self, query, alias, col, source, is_summary):
-        aggregate = SQLSum(col, source=source, is_summary=is_summary, **self.extra)
+        aggregate = SQLSum(col, source=source, is_summary=is_summary,
+                           **self.extra)
         query.aggregates[alias] = aggregate
